5a91e47a3a7d1a21a906be8a5b55c28d7e5cb543
IHU3025 <118242950+IHU3025@users.noreply.github.com>
2025-10-29 04:53:49 -0700

    changed prefab, added collider for default plane

---
+++ Assets/Scenes/script/PathHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathHolder : MonoBehaviour
+{
+    public List<string> folderPaths = new List<string>();
+
+}
